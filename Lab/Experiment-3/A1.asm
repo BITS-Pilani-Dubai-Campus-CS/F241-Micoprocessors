35503e47553b75b5ed0a6cea7381e2fefe1ca13f
mavericksxx <130738587+mavericksxx@users.noreply.github.com>
2024-03-01 10:20:09 +0400

    Update A1.asm

--- Lab/Experiment-3/A1.asm
+++ Lab/Experiment-3/A1.asm
@@ -1,43 +1,29 @@
-//Write a program to arrange the given array of 8-bit binary numbers stored in the memory in ascending order (NUM DB 95H, 85H, 75H, 65H, 55H, 45H, 35H, 25H)
+.MODEL SMALL
+.STACK 20
+.DATA ; Data segment starts here
+ORG 1000H ; Memory address initialization
+NUM DB 95H, 85H, 75H, 65H, 55H, 45H, 35H, 25H ; Eight 2-digit hex numbers input
+SUM DW ? ; Variable to store the sum
+COUNT DW 0008H ; Count variable to store the count of 8
 
-.model small
-.data
-NUM DB 95H, 85H, 75H, 65H, 55H, 45H, 35H, 25H
-SIZE EQU $ - NUM ; Calculate the size of the array
+.CODE ; Code starts here
+START:
+    MOV AX, @DATA ; Initializing DS: segment register
+    MOV DS, AX
+    MOV CX, COUNT ; Reg CX is initializing to COUNT=8
+    MOV SI, 0000H ; Initializing Source Index Register SI=0000
+    MOV AX, 0000H ; move AX=0000
 
-.code
-main proc
-    mov ax, @data
-    mov ds, ax
+REPEAT: ; Loop starts here
+    ADD AL, NUM[SI] ; Moving the 1st array value to AL register
+    JNC NEXT ; Jump If not Carry, here Next is a 16-bit address.
+    ADD AH, 01 ; Add 01 to AH
 
-    ; Outer loop for bubble sort
-    mov cx, SIZE
-outerLoop:
-    mov si, 0
-innerLoop:
-    mov al, NUM[si]
-    cmp al, NUM[si + 1]
-    jbe noSwap ; If not greater or equal, skip swapping
-    xchg al, NUM[si + 1]
-    mov NUM[si], al
-noSwap:
-    inc si
-    loop innerLoop
+NEXT: ; Next address reference
+    INC SI ; Increment SI
+    LOOP REPEAT ; Repeat loop reference
 
-    dec cx
-    jnz outerLoop
+    MOV SUM, AX ; Moving the AX value to the Sum variable memory
+    INT 3 ; Breakpoint interrupt
 
-    ; Display the sorted array
-    mov si, 0
-displayLoop:
-    mov dl, NUM[si]
-    add dl, 30h ; Convert to ASCII
-    mov ah, 02h ; Display character
-    int 21h
-    inc si
-    loop displayLoop
-
-    mov ah, 4Ch ; Exit program
-    int 21h
-main endp
-end main
+END START ; Stopping the program
